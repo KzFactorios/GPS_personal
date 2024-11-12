05993f512a2cb5d8e45182e9f259200e3da7013b
Robert Kurtz <rob@robkurtz.net>
2024-11-12 13:41:00 -0700

    fixed a bug where the inner frame was not being returned causing an empty display

--- prototypes/gui.lua
+++ prototypes/gui.lua
@@ -13,9 +13,9 @@ __wct_gps_gui.update_gui = function(_custom, player)
 end
 
 __wct_gps_gui.reset_gui = function(_custom, player)
-    if player then
-        destroy_common_mod_gui(player)
+    if player then        
         destroy_legacy_mod_gui(player)
+        destroy_common_mod_gui(player)
     end
 end
 
@@ -44,25 +44,36 @@ function get_parent_workspace(player)
                 return _inner
             end
         else
-            return create_legacy_parent_gui(player)
+            return create_parent_gui_failsafe(player)
         end
     end
     return nil
 end
 
-function create_legacy_parent_gui(player)
+-- if for some reason there is no structure to place our gui in, mimic and create
+function create_parent_gui_failsafe(player)
     if player then
-        local legacy_gui = wutils.tableFindByName(player.gui.top.children, _constants.LEGACYGPS_MODNAME)
-        if not legacy_gui then
-            legacy_gui = player.gui.top.add {
-                name = _constants.LEGACYGPS_MODNAME,
+        local _top = wutils.tableFindByName(player.gui.top.children, _constants.GUITOPFRAME)
+        if not _top then
+            _top = player.gui.top.add {
+                name = _constants.GUITOPFRAME,
                 type = "frame",
                 direction = "horizontal",
-                style = _constants.LEGACYGPS_MODNAME .. "_container",
+                style = "slot_window_frame"
             }
         end
 
-        return legacy_gui
+        local _inner = wutils.tableFindByName(_top.children, _constants.GUIINNERFRAME)
+        if not _inner then
+            _inner = _top.add {
+                name = _constants.GUIINNERFRAME,
+                type = "frame",
+                direction = "horizontal",
+                style = "mod_gui_inside_deep_frame"
+            }
+        end
+
+        return _inner
     end
     return nil
 end
@@ -79,11 +90,6 @@ function destroy_common_mod_gui(player)
                 end
             end
         end
-        --[[local _common = build_gui(player)
-        if _common then
-            _common.destroy()
-            return
-        end]]
     end
 end
 
@@ -93,26 +99,23 @@ function destroy_legacy_mod_gui(player)
         if legacy_gui then
             legacy_gui.destroy()
         end
-        -- remove any existence of old method
-        --[[for _, v in ipairs(player.gui.top.children) do
-            if v.name == _constants.LEGACYGPS_MODNAME then
-                v.destroy()
-                return
-            end
-        end]]
     end
 end
 
+-- ensures we have proper structure in place
 function build_gui(player)
     if player then
+        
         local parent = get_parent_workspace(player)
+        if parent and parent[_constants.GPS_MODNAME] then return parent[_constants.GPS_MODNAME] end
+
         if not parent then
             -- something went wrong
             -- build it the legacy way
             return nil
         end
-        -- so now we have a parent container
 
+        -- so now we have a parent container
         local gps
         if not parent[_constants.GPS_MODNAME] then
             gps = createGpsContainer(parent)
@@ -120,7 +123,6 @@ function build_gui(player)
             local latLine = createLatLine(gps)
         end
         -- so now we have built our gps
-
         return parent[_constants.GPS_MODNAME]
     end
 
